3b6ad9a58b5695a03440f523025c36f9ebc53203
Metron16 <metron16@gmail.com>
2021-03-19 23:53:29 +0100

    Segítség kikommentelése

--- fogyasztas.js
+++ fogyasztas.js
@@ -4,8 +4,8 @@ do
 {
     km = prompt("Hány kilométert mentél?")
     var szam_e = /\d+/g.test(km)
-    console.log(szam_e) 
-    console.log(km)
+    //console.log(szam_e) 
+    //console.log(km)
     
 
 } while(szam_e && km <= 1000)
