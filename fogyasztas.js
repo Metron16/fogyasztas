1cc47e5a559fb9fed2fdf584b308f3af5fc0b55d
Metron16 <metron16@gmail.com>
2021-03-20 12:58:18 +0100

    6. feladat javítása

--- fogyasztas.js
+++ fogyasztas.js
@@ -2,10 +2,9 @@
 let km
 do
 {
-    km = prompt("Hány kilométert mentél?")
-    var szam_e = /\d+/g.test(km)
+    km = prompt("Hány kilométert mentél?") 
+    var szam_e = /^-?\d+\.?\d*$/g.test(km) // Nem tetszett, hogy beírhattam a sor elejére és a sor végére betűket is a szám mellé, és akkor is elfogadta a megadott "számot", ezért keresnem kellett egy bonyolultabb kikötési rendszert, ez már nem fogadja el, ha talál valamilyen betűt bárhol, és engedi akár a 111.34-t is és hasonlókat.
     //console.log(szam_e) 
     //console.log(km)
-    
 
-} while(szam_e && km <= 1000)
+} while(!szam_e || km > 1000)
